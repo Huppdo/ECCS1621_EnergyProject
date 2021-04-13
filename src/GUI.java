2b2b18970086d3c5e900c129df8bcd1e6a56706a
Dominic Hupp <dhupp2020@gmail.com>
2021-04-13 14:11:21 -0400

    Screens 0 - 2

--- src/GUI.java
+++ src/GUI.java
@@ -2,7 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class GUI {
     private JPanel mainPanel;
@@ -12,33 +16,20 @@ public class GUI {
     private JButton nextTo3Button;
     private JPanel screen3;
     private JButton startAt1Button;
-    private JTextField roomName;
     private JButton addRoomButton;
     private JTextArea rooms;
+    private JPanel screen0;
+    private JButton startHomeEnergyCalculatorButton;
+    private JButton confirmButton;
+    private JTextArea bulbInfo;
     private JButton resetButtonScreen1;
+    private JTextField lightingHourTextField;
 
-    private static ArrayList<String> roomList;
+    private static HashMap<String, Integer> heating;
+    private static HashMap<Integer, Integer> lightingTypes;
+    private static HashMap<Integer, Integer> lightingHours;
 
-    /*
-    - Page 1: Collect titles of rooms
-    - Page 2: Collect exterior dimensions, height of one floor, # of floors
-    - Page 3: Window information
-    - Page 4: Heating information (# of heaters)
-    - Page 5: Providing Sensor Data
-    - Page 6: Confirm Lighting
-    - Page 7: Calculate Home Report & Potential Savings
-     */
-
-    /*
-    Room, Power Usage (W) from Baseboard
-    Living Room, 2000
-    Dining Room, 1500
-    Kitchen, 2000
-    Bedroom large, 2000
-    Bedroom medium2, 1700
-    Bedroom small, 1600
-    Bathroom, 1000
-     */
+    private static int lightingSlidePosition;
 
     public GUI() {
         nextButton.addActionListener(new ActionListener() {
@@ -46,7 +37,10 @@ public class GUI {
             public void actionPerformed(ActionEvent e) {
                 screen1.setVisible(false);
                 screen2.setVisible(true);
-                screen3.setVisible(false);
+
+                int currentKey = (int) lightingTypes.keySet().toArray()[lightingSlidePosition];
+                bulbInfo.setText("Out of 24 hours, how often do you use the " + currentKey + "W bulbs (on average)? You have "
+                        + lightingTypes.get(currentKey) + " in your house.");
             }
         });
         nextTo3Button.addActionListener(new ActionListener() {
@@ -65,36 +59,94 @@ public class GUI {
                 screen3.setVisible(false);
             }
         });
-        addRoomButton.addActionListener(new ActionListener() {
+        startHomeEnergyCalculatorButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                roomList.add(roomName.getText());
-                StringBuilder roomStr = new StringBuilder();
+                StringBuilder roomstr = new StringBuilder();
 
-                for (String str : roomList) {
-                    roomStr.append(str);
-                    roomStr.append('\n');
+                for (String str : heating.keySet()) {
+                    roomstr.append(str);
+                    roomstr.append('\n');
                 }
 
-                rooms.setText(roomStr.toString());
-
-                System.out.println(roomList);
-
-                roomName.setText("");
+                rooms.setText(roomstr.toString());
+                screen0.setVisible(false);
+                screen1.setVisible(true);
             }
         });
-        resetButtonScreen1.addActionListener(new ActionListener() {
+        confirmButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                roomName.setText("");
-                rooms.setText("");
-                roomList.clear();
+                if (lightingSlidePosition < lightingTypes.keySet().size()) {
+
+                    String hourValueStr = lightingHourTextField.getText();
+                    int hourValue = -1;
+
+                    try {
+                        float temp = Float.parseFloat(hourValueStr);
+                        hourValue = Math.round(temp);
+                    } catch (Exception ex1) {
+                        String currentInfo = bulbInfo.getText();
+                        currentInfo += "\n\n Please enter a number for the hours (ex. 18)";
+                        bulbInfo.setText(currentInfo);
+                        return;
+                    }
+
+                    lightingHours.put((int) lightingTypes.keySet().toArray()[lightingSlidePosition], hourValue);
+                    lightingHourTextField.setText("");
+                    lightingSlidePosition += 1;
+                    if (lightingSlidePosition < lightingTypes.keySet().size()) {
+                        int currentKey = (int) lightingTypes.keySet().toArray()[lightingSlidePosition];
+                        bulbInfo.setText("Out of 24 hours, how often do you use the " + currentKey + "W bulbs (on average)? You have "
+                                + lightingTypes.get(currentKey) + " in your house.");
+                    } else {
+                        bulbInfo.setText("Data has been inputted for all of the lightbulbs in your home. Please continue to the next page");
+                        confirmButton.setEnabled(false);
+                        lightingHourTextField.setVisible(false);
+                        confirmButton.setVisible(false);
+                    }
+                }
             }
         });
     }
 
     public static void main(String[] args) {
-        roomList = new ArrayList<String>();
+        heating = new HashMap<String, Integer>();
+        lightingTypes = new HashMap<Integer, Integer>();
+        lightingHours = new HashMap<Integer, Integer>();
+
+        lightingSlidePosition = 0;
+
+        File sensorData = new File("src/dataFromSensors.txt");
+        Scanner dataScanner;
+
+        try {
+            dataScanner = new Scanner(sensorData);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Error loading dataFromSensors.txt, exiting program.");
+            return;
+        }
+
+        boolean header = true;
+        boolean readingLighting = false;
+        while (dataScanner.hasNextLine()) {
+            String data = dataScanner.nextLine();
+            if (header) {
+                header = false;
+            } else {
+                if (data.isBlank()) {
+                    readingLighting = !readingLighting;
+                    header = true;
+                } else {
+                    String[] dataSplit = data.split(", ");
+                    if (!readingLighting) {
+                        heating.put(dataSplit[0], Integer.parseInt(dataSplit[1]));
+                    } else {
+                        lightingTypes.put(Integer.parseInt(dataSplit[0]), Integer.parseInt(dataSplit[1]));
+                    }
+                }
+            }
+        }
 
         JFrame myFrame = new JFrame("Mental Health Resources Simulation");
         myFrame.setContentPane(new GUI().mainPanel);
@@ -108,6 +160,5 @@ public class GUI {
         // put everything in the frame and make it visible
         myFrame.pack();
         myFrame.setVisible(true);
-
     }
 }
